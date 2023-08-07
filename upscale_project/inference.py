e0a2882179aba19e393408381a468000c05833e5
5yearsKim <hypothesis22@naver.com>
2023-08-07 18:17:36 +0900

    modify code

--- upscale_project/inference.py
+++ upscale_project/inference.py
@@ -4,24 +4,24 @@ from torch.nn import functional as F
 import os
 import cv2
 import numpy as np
-from upcunet_v3 import onoma_upscaler 
+from onoma_upscaler import RealWaifuUpScaler
 
 
-
+ModelPath = '/Data2/spacewebui/ailab/Real-CUGAN/model_weights/up2x-latest-no-denoise.pth'
 
 upscaler = RealWaifuUpScaler(2, ModelPath, half=True, device="cuda:0")
 
 
-process_image_path(image_target, scale=2, tile=Tile, save=True, save_dir = os.path.join(FinishPath,"result.png")) # call this to inference
-
-    
-def process_image_path(path, scale=2, tile=5, save=False, save_dir = 'finish/result.png'):
+def process_image_path(path, scale=2, save=False, save_dir = 'finish/result.png'):
     img = cv2.imread(path)[:, :, [2, 1, 0]]
 
     torch.cuda.empty_cache()
-    result = upscaler(img, tile_mode=tile, cache_mode=2, alpha=1)
+    result = upscaler(img, tile_mode=5, cache_mode=2, alpha=1)
 
     if save:
         cv2.imwrite(save_dir, result[:, :, [2, 1, 0]])
 
     return result
+
+
+process_image_path('./test.png', scale=2, save=True, save_dir = './result.png') # call this to inference
